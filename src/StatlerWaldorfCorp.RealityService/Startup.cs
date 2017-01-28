da943baad9abd4b946654e269b743b41be7a8434
Kevin Hoffman <alothien@gmail.com>
2017-01-28 11:13:55 -0500

    Initial commit

---
+++ src/StatlerWaldorfCorp.RealityService/Startup.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Steeltoe.Extensions.Configuration;
+using Steeltoe.Extensions.Configuration.CloudFoundry;
+using Steeltoe.CloudFoundry.Connector.Redis;
+using Steeltoe.CloudFoundry.Connector.Services;
+
+
+namespace StatlerWaldorfCorp.RealityService
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env, ILoggerFactory loggerFactory) 
+        {
+            loggerFactory.AddConsole();
+            loggerFactory.AddDebug();
+            
+            var builder = new ConfigurationBuilder()                
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: false)
+		        .AddEnvironmentVariables()		                    
+                .AddCloudFoundry();
+
+	        Configuration = builder.Build();    		        
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services) 
+        {
+            services.AddMvc();
+            services.AddOptions();
+
+            services.Configure<CloudFoundryApplicationOptions>(Configuration);
+            services.Configure<CloudFoundryServicesOptions>(Configuration);            
+            
+            services.AddRedisConnectionMultiplexer(Configuration);                               
+        }
+      
+        public void Configure(IApplicationBuilder app, 
+                IHostingEnvironment env, 
+                ILoggerFactory loggerFactory) 
+        {                                   
+            app.UseMvc();        
+        }
+    }
+}
